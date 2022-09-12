08c90d317f828ef1959ea6178f02a7ab09ef990a
Sergejs Germanovs <sergejs.germanovs@edigard.com>
2022-09-12 19:10:17 +0300

    Add 2 tests
    Move max up, move max down

--- tests/KeysMoverTest.php
+++ tests/KeysMoverTest.php
@@ -6,6 +6,16 @@ class KeysMoverTest extends TestCase {
 
 	public function testMoveKey()
 	{
+
+		$this->moveUp();
+		$this->moveUpMax();
+		$this->moveDown();
+		$this->moveDownMax();
+
+	}
+
+	protected function moveUp()
+	{
 		$mover = new KeysMover\KeysMover;
 		$source = [
 			'one' => 1,
@@ -14,10 +24,9 @@ class KeysMoverTest extends TestCase {
 			'four' => 4,
 			'five' => 5
 		];
-		
+
 		// test 1: common, up, targets by names
-		$test = $source;
-		$mover->moveKey($test, 'four', 'two');
+		$mover->moveKey($source, 'four', 'two');
 		$expected = [
 			'one' => 1,
 			'four' => 4,
@@ -25,11 +34,22 @@ class KeysMoverTest extends TestCase {
 			'three' => 3,
 			'five' => 5
 		];
-		$this->assertSame($expected, $test);
-		
+		$this->assertSame($expected, $source);
+	}
+
+	protected function moveDown()
+	{
 		// test 2: common, down, targets by names
-		$test = $source;
-		$mover->moveKey($test, 'two', 'four');
+		$mover = new KeysMover\KeysMover;
+		$source = [
+			'one' => 1,
+			'two' => 2,
+			'three' => 3,
+			'four' => 4,
+			'five' => 5
+		];
+
+		$mover->moveKey($source, 'two', 'four');
 		$expected = [
 			'one' => 1,
 			'three' => 3,
@@ -37,7 +57,53 @@ class KeysMoverTest extends TestCase {
 			'two' => 2,
 			'five' => 5
 		];
-		$this->assertSame($expected, $test);
+		$this->assertSame($expected, $source);
+	}
+	
+	protected function moveUpMax()
+	{
+		// test 3: common, max down, targets by names
+		$mover = new KeysMover\KeysMover;
+		$source = [
+			'one' => 1,
+			'two' => 2,
+			'three' => 3,
+			'four' => 4,
+			'five' => 5
+		];
+
+		$mover->moveKey($source, 'five', 'one');
+		$expected = [
+			'five' => 5,
+			'one' => 1,
+			'two' => 2,
+			'three' => 3,
+			'four' => 4
+		];
+		$this->assertSame($expected, $source);
+	}
+
+	protected function moveDownMax()
+	{
+		// test 4: common, max down, targets by names
+		$mover = new KeysMover\KeysMover;
+		$source = [
+			'one' => 1,
+			'two' => 2,
+			'three' => 3,
+			'four' => 4,
+			'five' => 5
+		];
+
+		$mover->moveKey($source, 'one', 'five');
+		$expected = [
+			'two' => 2,
+			'three' => 3,
+			'four' => 4,
+			'five' => 5,
+			'one' => 1
+		];
+		$this->assertSame($expected, $source);
 	}
 
 }
